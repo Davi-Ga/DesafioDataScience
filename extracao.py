78d79de8bb00baa50c4df4b94a1e6fea09cf1585
Davi-Ga <davigaldinoky@gmail.com>
2022-04-05 19:32:07 -0300

    Teste

---
+++ extracao.py
@@ -0,0 +1,9 @@
+from google.colab import drive
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+dados=pd.read_csv('/content/drive/My Drive/houses_to_rent_v2.csv')
+
+dados.head(10)
+
+profile = ProfileReport(dados, title='Dados de alugueis')
