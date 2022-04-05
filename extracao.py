02beba71220099c5dd0b31884bf75fe292222c57
Davi-Ga <davigaldinoky@gmail.com>
2022-04-05 19:37:33 -0300

    Adição da HEAD

--- extracao.py
+++ extracao.py
@@ -6,4 +6,5 @@ dados=pd.read_csv('/content/drive/My Drive/houses_to_rent_v2.csv')
 
 dados.head(10)
 
-profile = ProfileReport(dados, title='Dados de alugueis')
+profile = ProfileReport(dados, title='Dados de alugueis',html={'style':{'full_width':True}})
+profile.to_notebook_iframe()
